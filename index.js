3ea654127029d4efca0db928dd4d2070476529cb
vennelalingam <lingamvennela@gmail.com>
2023-05-07 18:33:27 +0530

    index.js_functions

--- index.js
+++ index.js
@@ -7,6 +7,13 @@ the value of x multiplied
 by three.
 */
 
+function triple(x){
+    return (x * 3);
+}
+let multiply = triple(7);
+console.log("Triple Multiplication:", multiply); //21
+
+
 /*
 2)
 Create a function named "subtract" that
@@ -14,16 +21,29 @@ takes two parameters and returns the result of
 the second value subtracted from the first.
 */
 
+function subtract(x, y){
+    return (x - y);
+}
+let result = subtract(17, 5)
+console.log("subtracted value:", result)
 
 /*
 3)
 Create a function named "createArray" that
 takes three arguments and returns
 those arguments in an array.
-
-Test case:
-console.log(createArray(3, "a", true)); // [3, "a", true]
 */
+
+let newArray = [];
+function createArray(arr1, arr2, arr3){
+   let result = newArray.push(arr1, arr2, arr3);
+    // console.log(newArray);
+    return newArray
+}
+
+// //Test case:
+// console.log(createArray(3, "a", true)); // [3, "a", true]
+// console.log(createArray(3, 4, 5));
 
 /*
 4)
@@ -33,11 +53,17 @@ string as part of a longer sentence that says hello.
 If the function is called with an argument that is
 not a string, it should return "Please provide a name."
 
-Test cases:
-console.log(greetPerson("Sofia")); // "Hello, Sofia!"
-console.log(greetPerson(42)); // "Please provide a name."
 */
+function greetPerson(aString){
+    if(typeof aString === 'string')
+    return (`Hello, ${aString}!`);
+    else
+    return (`Please provide a name`);
+}
 
+// // Test cases:
+// console.log(greetPerson("Sofia")); // "Hello, Sofia!"
+// console.log(greetPerson(42)); // "Please provide a name."
 
 /*
 5)
@@ -46,9 +72,17 @@ in an array of numbers as an argument
 and returns the sum of all those numbers
 added together.
 
-Test case:
-console.log(addTogether([1, 2, 3, 4, 5])); // 15
 */
+function addTogether(numArr){
+    let sum = 0;
+    for(let i=0; i<numArr.length; i++){
+         sum += numArr[i]
+        console.log(sum);          
+    }
+    return sum;
+}
+//Test case:
+// console.log(addTogether([1, 2, 3, 4, 5])); // 15
 
 /*
 6)
@@ -60,12 +94,18 @@ found, it should return -1.
 
 Do NOT use the array method indexOf
 in your answer. Hint: Create a loop!
-
-Test cases:
-console.log(getIndex([1, 2, 3, 4, 5, 6], 4)); // 3
-console.log(getIndex([1, 2, 3, 4, 5, 6], 7)); // -1
 */
 
+function getIndex(arr1, val1){
+
+    let result = arr1.indexOf(val1)
+    return result;
+    
+}
+
+// // Test cases:
+// console.log(getIndex([1, 2, 3, 4, 5, 6], 4)); // 3
+// console.log(getIndex([1, 2, 3, 4, 5, 6], 7)); // -1
 
 /*
 7)
@@ -82,6 +122,27 @@ Test cases:
 console.log(includesValue([1, 2, 3, 4, 5, 6], 4)); // true
 console.log(includesValue([1, 2, 3, 4, 5, 6], 7)); // false
 */
+
+function includesValue(arr1, val1){
+
+// calling inner function before it is declared => "hoisting"
+    const result = getIndex(arr1, val1);
+
+    function getIndex(arr1, val1){
+        return arr1.indexOf(val1)
+    }
+   
+    if (result >= 0)
+    return true
+    else 
+    return false
+    
+}    
+
+// Test cases:
+console.log(includesValue([1, 2, 3, 4, 5, 6], 7)); // true
+console.log(includesValue([1, 2, 3, 4, 5, 6], 6)); // false
+
 
 /*
 8) Nested Objects
